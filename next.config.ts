e0e01c58aef677b131be471be99b452c24c68485
Pandora <carlofer.braza@hcdc.edu.ph>
2025-11-09 22:41:06 +0800

    lint fixed again

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Exclude Firebase Functions directory from Next.js build
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/functions/**'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
